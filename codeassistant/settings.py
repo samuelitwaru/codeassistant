263c8b94dcd1ea40610b6a39964508474da355ac
samuelitwaru <samuelitwaru@gmail.com>
2024-01-06 13:48:03 +0300

    3 commit

--- codeassistant/settings.py
+++ codeassistant/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'codeassistant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db.sqlite3',
     }
 }
 
