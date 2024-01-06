dde15230e41a7f1e695a03d8e7800123e8f3192d
samuelitwaru <samuelitwaru@gmail.com>
2024-01-06 13:52:46 +0300

    add * on allowed hosts

--- codeassistant/settings.py
+++ codeassistant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1p55-e1q=#vz-!kki5hgyk54db-6wyyf(83$)v!%narl@$m#i_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
